6b73d9957e7baec70c1afd254f2439020ab6da16
thousand21 <thousand24@outlook.fr>
2021-06-20 21:46:52 +0200

    fini

--- src/java/main.js
+++ src/java/main.js
@@ -34,9 +34,67 @@ let btnNoir=document.getElementsByClassName("noir")[0];
 btnNoir.addEventListener("click",()=>{
 body.removeAttribute("style");
 body.style.backgroundColor="black";
+body.style.color="white";
 })
 let btnBlanc=document.getElementsByClassName("blanc")[0];
 btnBlanc.addEventListener("click",()=>{
     body.removeAttribute("style");
     body.style.backgroundColor="white";
+    body.style.color="black";
+    })
+
+    ///////////////////////
+    let pancarteGris=document.getElementsByClassName("grossePancarteGrise")[0];
+    let divGris=document.getElementsByClassName("ensembleGris")[0];
+    let btnConnexion=document.getElementsByClassName("connexion")[0];
+    let formInscription=document.getElementsByClassName("inscription")[0];
+    let formConnection=document.getElementsByClassName("formConnection")[0];
+    let formCache=document.getElementsByClassName("formulaireCache")[0];
+    btnConnexion.addEventListener("click",()=>{
+       
+        formCache.classList.remove("formulaireCache");
+        
+        formInscription.classList.remove("inscription");
+       
+        body.classList.toggle("opacity");
+       
+        
+        pancarteGris.classList.toggle("opacity");
+    })
+
+    ///////////////////////////////////
+    
+    let btnformConnection=document.getElementsByClassName("btnFormConnection")[0];
+    let btnInscription=document.getElementsByClassName("btnFormInscription")[0];
+    btnformConnection.addEventListener("click",()=>{
+        formInscription.classList.add("inscription");
+        formConnection.classList.remove("formConnection");
+
+    })
+    btnInscription.addEventListener("click",()=>{
+        formInscription.classList.remove("inscription");
+        formConnection.classList.add("formConnection");
+        console.log("8");
+    })
+
+    ///////////////////croix fermer formulaire
+    let croix1=document.getElementsByTagName("i")[0];
+    let croix2=document.getElementsByTagName("i")[5];
+    croix1.addEventListener("click",()=>{
+        formCache.classList.add("formulaireCache");
+        formInscription.classList.add("inscription");
+        formConnection.classList.add("formConnection");
+        body.classList.remove("opacity");
+       
+        
+        pancarteGris.classList.remove("opacity");
+    })
+    croix2.addEventListener("click",()=>{
+        formCache.classList.add("formulaireCache");
+        formInscription.classList.add("inscription");
+        formConnection.classList.add("formConnection");
+        body.classList.remove("opacity");
+       
+        
+        pancarteGris.classList.remove("opacity");
     })
